e0134eee3cb3e1af28011c585562dc8ba4bcab02
Your Name <you@example.com>
2018-10-05 10:02:53 +1000

    add version 1

---
+++ db/migrate/20181003054254_create_actors.rb
@@ -0,0 +1,12 @@
+class CreateActors < ActiveRecord::Migration[5.2]
+  def change
+    create_table :actors do |t|
+      t.string :name
+      t.integer :age
+      t.string :gender
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
