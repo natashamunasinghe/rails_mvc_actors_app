398876e556d0303b36faa33540d6cb0f68b37642
Your Name <you@example.com>
2018-10-15 16:03:16 +1100

    add seed information

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |x|
+    Actor.create(
+        name: Faker::GameOfThrones.character,
+        age: rand(50),
+        gender: "male"
+    )
+    
+    end
+    
